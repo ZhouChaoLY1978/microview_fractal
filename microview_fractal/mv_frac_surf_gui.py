aa9a6d5ee6172829596fb184b9e16daeeb05769f
Administrator <zandz1978@126.com>
2020-10-12 15:56:20 +0800

    小改动

--- microview_fractal/mv_frac_surf_gui.py
+++ microview_fractal/mv_frac_surf_gui.py
@@ -60,14 +60,14 @@ class MvFractalSurfaceGui(QMainWindow):
         ''' 选择建模算法 '''
         gp_method = QGroupBox("选择建模算法")
         gp_method.setMaximumHeight(120)
-        self.radio_dft = QRadioButton("离散傅里叶逆变换法")
+        self.radio_dft = QRadioButton("离散傅里叶逆变换")
         self.radio_rmd = QRadioButton("随机中点位移法")
         self.radio_wm = QRadioButton("W-M函数法")
         self.radio_dft.setChecked(True)
 
-        self.radio_dft.toggled.connect(self._select_method)
-        self.radio_rmd.toggled.connect(self._select_method)
-        self.radio_wm.toggled.connect(self._select_method)
+        self.radio_dft.toggled.connect(lambda: self._select_method(self.radio_dft))
+        self.radio_rmd.toggled.connect(lambda: self._select_method(self.radio_rmd))
+        self.radio_wm.toggled.connect(lambda: self._select_method(self.radio_wm))
 
         layout = QVBoxLayout()
         layout.addWidget(self.radio_dft)
@@ -76,14 +76,37 @@ class MvFractalSurfaceGui(QMainWindow):
         gp_method.setLayout(layout)
         self.right_layout.addWidget(gp_method)
 
-    def _select_method(self):
-        sender = self.sender()
-        if sender.text() == "离散傅里叶逆变换法":
-            print(sender.text())
-        elif sender.text() == "随机中点位移法":
-            print(sender.text())
+    def _select_method(self, btn_method):
+        if btn_method.isChecked():
+            self.method = btn_method.text()
+        if self.method == "离散傅里叶逆变换":
+            self.lab_sq.setText("高度Sq：")
+            self.chx_stable.setText("平稳分形")
+            self.chx_stable.setDisabled(False)
+            self.chx_stable.setChecked(True)
+        elif self.method == "随机中点位移法":
+            self.lab_sq.setText("尺度系数：")
+            self.chx_stable.setDisabled(True)
+            self.chx_stable.setChecked(False)
         else:
-            print(sender.text())
+            self.lab_sq.setText("尺度系数：")
+            self.chx_stable.setText("随机相位")
+            self.chx_stable.setDisabled(False)
+            self.chx_stable.setChecked(True)
+
+    def _set_get_para(self, method="离散傅里叶逆变换"):
+        para_group = QGroupBox("输入建模参数")
+        para_group.setMaximumHeight(150)
+        # 设置4个标签 --------
+        self.lab_dim = QLabel("分形维数：")
+
+        if method == "离散傅里叶逆变换":
+            self.lab_sq = QLabel("高度Sq：")
+        else:
+            self.lab_sq = QLabel("尺度系数：")
+
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
